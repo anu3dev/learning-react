1cf70c42443229f2e19220c2273409d2f1c315cc
anurag kumar <anu3dev@gmail.com>
2023-06-11 17:51:06 -0500

    adding code for react-router-dom 5.2.0

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,6 +4,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 const HomeScreen = lazy(() => import('../components/molecules/HomeScreen'))
 const ContactUsScreen = lazy(() => import('../components/molecules/ContactUsScreen'))
 
+// in case of react-router-dom = 5.2.0
+
+// const RouteConfig = ({ match }) => {
+//   let selectedRoute = null
+
+//   switch (match.params[0]) {
+//     case 'home':
+//       selectedRoute = <HomeScreen />
+//       break
+//     case 'contact':
+//       selectedRoute = <ContactUsScreen />
+//       break
+//     default:
+//       selectedRoute = <HomeScreen />
+//       break
+//   }
+//   return selectedRoute
+// }
+
 const RouteConfig = () => {
   let selectedRoute = null
 
@@ -22,6 +41,21 @@ const RouteConfig = () => {
 }
 
 const routeBasePath = '/*'
+
+// in case of react-router-dom = 5.2.0
+
+// const routes = (
+//   <BrowserRouter>
+//     <Switch>
+//       <Route
+//         path={routeBasePath}
+//         render={(props) => {
+//           return <RouteConfig {...props} />
+//         }}
+//       />
+//     </Switch>
+//   </BrowserRouter>
+// )
 
 const routes = (
   <BrowserRouter>
